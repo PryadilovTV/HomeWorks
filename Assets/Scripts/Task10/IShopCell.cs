19cb4f6f28089b79531ecec6f86f3f09eb11d7d9
PryadilovTV <pryadilovtv@gmail.com>
2021-08-02 12:28:45 +0400

    no message

--- Assets/Scripts/Task10/IShopCell.cs
+++ Assets/Scripts/Task10/IShopCell.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public abstract class IShopCell : MonoBehaviour
+public abstract class IShopCell : AbstractShopCell
 {
     public Button _button;
     
