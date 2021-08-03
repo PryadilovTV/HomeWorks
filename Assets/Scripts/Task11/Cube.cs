7dcd95b82e8b87451305e40cc02e7f117bfd01b3
PryadilovTV <pryadilovtv@gmail.com>
2021-08-03 11:53:17 +0400

    Таска 11. Не закончена

---
+++ Assets/Scripts/Task11/Cube.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cube : MonoBehaviour
+{
+    [SerializeField] private float _limitScale;
+    void Update()
+    {
+        var changeScale = new Vector3(0.005f, 0.005f, 0.005f);
+        //var changeRota
+        
+        transform.localScale += changeScale;
+        
+        if (transform.localScale.x >= _limitScale) Destroy(gameObject);
+    }
+}
---
+++ Assets/Scripts/Task11/TaskManager.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class TaskManager : MonoBehaviour
+{
+    [SerializeField] private GameObject _cubePrefab;
+    [SerializeField] private int _countCubes;
+
+    void Start()
+    {
+        for (int i = 1; i < _countCubes; i++)
+        {
+            Invoke("CreateCube", i);
+        }
+    }
+
+    void CreateCube()
+    {
+        var pos = new Vector3((float) Random.Range(-40, 40), (float) Random.Range(-30, 30), 0);
+        GameObject newCube = Instantiate(_cubePrefab);
+        newCube.transform.position = pos;
+        //newCube.transform.rotation = Quaternion.identity;
+    }
+
+    private void Update()
+    {
+        {
+            if (Input.GetMouseButtonDown(0)){
+            
+                var hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition),Vector2.zero);
+
+                if (hit.collider.CompareTag("Cube")) 
+                {
+                   Debug.Log("Cube pushed");
+                }
+            }
+     
+        }
+    }
+}
