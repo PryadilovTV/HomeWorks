6144766db7acb7041c23140f7c2872d0fc486590
PryadilovTV <pryadilovtv@gmail.com>
2021-08-03 10:52:16 +0400

    Таска 8, не работает!

--- Assets/Scripts/Task8/Task8.cs
+++ Assets/Scripts/Task8/Task8.cs
@@ -36,13 +36,20 @@ public class Task8 : MonoBehaviour
             
             //transform.DOMove(Input.mousePosition, Duration);
 
+            /*
             Debug.Log(Input.mousePosition);
             var screenPosition = new Vector3(Input.mousePosition.x, Input.mousePosition.y, transform.position.z);
             Debug.Log(screenPosition);
             var worldPosition = Camera.main.ScreenToWorldPoint(screenPosition);
             Debug.Log(worldPosition);
-            transform.DOMove(new Vector3(worldPosition.x, worldPosition.y, transform.position.z), Duration);
-            
+            transform.DOMove(worldPosition, Duration);
+            */
+            var camera = Camera.main;
+            var mousePos2D = Input.mousePosition;
+            var point = new Vector3(mousePos2D.x, mousePos2D.y, transform.position.z);
+            var worldPointPos = camera.ScreenToWorldPoint(point);
+            Debug.LogAssertion($"{mousePos2D}, {worldPointPos}");
+            transform.DOMove(worldPointPos, Duration);
             
         }    
     }
